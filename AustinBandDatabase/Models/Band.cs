1018ac143346573f50e8725ea0ef55e5e37754e5
Andrew Storrow <andrew.storrow@gmail.com>
2018-03-23 16:43:52 -0500

    adding jquery

--- AustinBandDatabase/Models/Band.cs
+++ AustinBandDatabase/Models/Band.cs
@@ -7,12 +7,24 @@ namespace AustinBandDatabase.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
-        [Display(Name = "Year Formed")]
+        [Display(Name = "Year Formed"), Required]
         public string YearFormed { get; set; }
 
-        [Display(Name = "Description")]
+        [Display(Name = "Description"), Required]
         public string DescriptionHtml { get; set; }
+
+        public Band()
+        {}
+
+        public Band(int id, string name, string yearFormed, string descriptionHtml)
+        {
+            Id = id;
+            Name = name;
+            YearFormed = yearFormed;
+            DescriptionHtml = descriptionHtml;
+        }
     }
 }
